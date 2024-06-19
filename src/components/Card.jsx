d2ddf6176f0cf77e59504c59bed873949903597a
MirkoCherchi <mirkocherchi1992@gmail.com>
2024-06-19 16:18:32 +0200

    feat: multifields

---
+++ src/components/Card.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { TiDeleteOutline as Delete } from "react-icons/ti";
+import { FiEdit as Edit } from "react-icons/fi";
+
+const Card = ({ post, onDelete, onEdit }) => {
+  const { title, content, category, tags, image, published } = post;
+
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+      <div className="card-content">
+        {image && <img src={image} alt={title} />}
+        <p>{content}</p>
+      </div>
+      <div className="card-footer">
+        <p>Category: {category}</p>
+        <p>
+          Tags:{" "}
+          {tags.map((tag, index) => (
+            <span className="tag" key={index}>
+              {"#"}
+              {tag}{" "}
+            </span>
+          ))}
+        </p>
+        <p>{published ? "Published" : "Not Published"}</p>
+        <div className="btnFlex">
+          <button onClick={onEdit} className="btnEdit">
+            <Edit className="iconEdit" />
+          </button>
+          <button onClick={onDelete} className="btnDelete">
+            <Delete className="iconDelete" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,25 +1,80 @@
 import { useState } from "react";
 import { TiDeleteOutline as Delete } from "react-icons/ti";
 import { FiEdit as Edit, FiSave as Save } from "react-icons/fi";
+import Card from "./Card";
 
 const Form = () => {
-  const [postTitle, setPostTitle] = useState("");
-  const [postArray, setPostArray] = useState([]);
-  const [editIndex, setEditIndex] = useState(null);
-  const [editTitle, setEditTitle] = useState("");
+  const listaTags = [
+    "Viaggio",
+    "Cucina",
+    "Fitness",
+    "Arte",
+    "Musica",
+    "Moda",
+    "Sport",
+    "Salute",
+    "Film",
+    "Libri",
+    "Fotografia",
+    "Natura",
+    "Casa e Giardino",
+    "Animazione",
+    "Storia",
+  ];
+  const listaCategorie = [
+    "Tecnologia",
+    "Notizie",
+    "Tutorial",
+    "Viaggi",
+    "Cucina",
+    "Sport",
+    "Musica",
+    "Film e TV",
+    "Libri",
+    "Salute e benessere",
+    "Moda",
+    "Finanza",
+    "Automobili",
+    "Fai da te",
+    "Animazioni e fumetti",
+  ];
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const [postArray, setPostArray] = useState([]);
+  const [formData, setFormData] = useState({
+    title: "",
+    content: "",
+    category: "",
+    tags: [],
+    image: "",
+    published: false,
+  });
+  const [editIndex, setEditIndex] = useState(null);
+
+  const handleField = (name, value) => {
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
     if (editIndex !== null) {
       const updatedPosts = [...postArray];
-      updatedPosts[editIndex] = editTitle;
+      updatedPosts[editIndex] = { ...formData };
       setPostArray(updatedPosts);
       setEditIndex(null);
-      setEditTitle("");
     } else {
-      setPostArray([...postArray, postTitle]);
-      setPostTitle("");
+      setPostArray([...postArray, { ...formData }]);
     }
+    setFormData({
+      title: "",
+      content: "",
+      category: "",
+      tags: [],
+      image: "",
+      published: false,
+    });
   };
 
   const handleDelete = (index) => {
@@ -29,25 +84,81 @@ const Form = () => {
 
   const handleEdit = (index) => {
     setEditIndex(index);
-    setEditTitle(postArray[index]);
+    const postToEdit = postArray[index];
+    setFormData({
+      title: postToEdit.title,
+      content: postToEdit.content,
+      category: postToEdit.category,
+      tags: postToEdit.tags,
+      image: postToEdit.image,
+      published: postToEdit.published,
+    });
   };
 
   return (
     <>
       <div className="container">
         <form onSubmit={handleSubmit}>
-          <label htmlFor="title">
-            {editIndex !== null ? "Modifica Post" : "Inserisci Post"}
-          </label>
           <input
+            name="title"
             type="text"
-            value={editIndex !== null ? editTitle : postTitle}
-            onChange={(e) =>
-              editIndex !== null
-                ? setEditTitle(e.target.value)
-                : setPostTitle(e.target.value)
-            }
+            placeholder="Title"
+            value={formData.title}
+            onChange={(e) => handleField("title", e.target.value)}
           />
+          <textarea
+            name="content"
+            placeholder="Content"
+            value={formData.content}
+            onChange={(e) => handleField("content", e.target.value)}
+          />
+          <select
+            name="category"
+            value={formData.category}
+            onChange={(e) => handleField("category", e.target.value)}
+          >
+            <option value="">Select Category</option>
+            {listaCategorie.map((category, index) => (
+              <option key={index} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+          <div>
+            <label>Tags:</label>
+            {listaTags.map((tag, tagIndex) => (
+              <label key={tagIndex}>
+                <input
+                  type="checkbox"
+                  checked={formData.tags.includes(tag)}
+                  onChange={() => {
+                    const currentTags = formData.tags;
+                    const updatedTags = currentTags.includes(tag)
+                      ? currentTags.filter((t) => t !== tag)
+                      : [...currentTags, tag];
+                    handleField("tags", updatedTags);
+                  }}
+                />
+                {tag}
+              </label>
+            ))}
+          </div>
+          <input
+            name="image"
+            type="text"
+            placeholder="Image URL"
+            value={formData.image}
+            onChange={(e) => handleField("image", e.target.value)}
+          />
+          <label>
+            <input
+              name="published"
+              type="checkbox"
+              checked={formData.published}
+              onChange={(e) => handleField("published", e.target.checked)}
+            />
+            Published
+          </label>
           <button
             className={`btnSubmit ${editIndex !== null ? "btnSave" : ""}`}
             type="submit"
@@ -59,18 +170,11 @@ const Form = () => {
           <ul>
             {postArray.map((post, index) => (
               <li className="listItem" key={index}>
-                <h5 className="postTitle">{post}</h5>
-                <div className="btnFlex">
-                  <button onClick={() => handleEdit(index)} className="btnEdit">
-                    <Edit className="iconEdit" />
-                  </button>
-                  <button
-                    onClick={() => handleDelete(index)}
-                    className="btnDelete"
-                  >
-                    <Delete className="iconDelete" />
-                  </button>
-                </div>
+                <Card
+                  post={post}
+                  onDelete={() => handleDelete(index)}
+                  onEdit={() => handleEdit(index)}
+                />
               </li>
             ))}
           </ul>
